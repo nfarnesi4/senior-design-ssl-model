51c366cc49832508a2d543e7aec8eb7a3cf36b48
Reno Farnesi <nfarnesi4@gmail.com>
2020-05-17 13:37:05 -0400

    Adds 3D animation and video saving

--- ver_test.py
+++ ver_test.py
@@ -12,6 +12,7 @@ import tensorflow as tf
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 import time
 
@@ -65,14 +66,42 @@ errors = abs(responses-responses_p)
 avg_error = sum(errors)/len(errors)
 print(avg_error)
 
+def update(num, data, scatters):
+    res, rep = data
+    scatters[0][0].set_data_3d(res[:num,0], res[:num,1], res[:num,2])
+    scatters[1]._offsets3d = ([res[num,0]], [res[num,1]], [res[num,2]])
+    scatters[2]._offsets3d = ([rep[num,0]], [rep[num,1]], [rep[num,2]])
+    return scatters
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(responses[:,0], responses[:,1], responses[:,2])
-ax.scatter(responses_p[:,0], responses_p[:,1], responses_p[:,2])
-plt.title('SSL Tracking Test: Spiral')
-plt.legend(['ground truth', 'estimated'])
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+#ax.scatter(responses[:,0], responses[:,1], responses[:,2])
+#ax.scatter(responses_p[:,0], responses_p[:,1], responses_p[:,2])
+plt.title('SSL Tracking')
+plt.legend(['sound source path', 'sound source', 'estimated'])
+ax.set_xlabel('x (m)')
+ax.set_ylabel('y (m)')
+ax.set_zlabel('z (m)')
+ax.set_xlim3d([0.0, 10.0])
+ax.set_ylim3d([0.0, 10.0])
+ax.set_zlim3d([-0.5, 2.5])
+ax.view_init(10,10)
+
+scatters = [ ax.plot([responses_p[0][0]], [responses_p[0][1]], [responses_p[0][2]]),
+             ax.scatter([responses_p[0][0]], [responses_p[0][1]], [responses_p[0][2]]),
+             ax.scatter([responses_p[0][0]], [responses_p[0][1]], [responses_p[0][2]])]
+
+data=(responses, responses_p)
+# Creating the Animation object
+ani = animation.FuncAnimation(fig, update, len(responses), fargs=(data, scatters),
+                              interval=1000/60, blit=False)
+
+plt.legend(['sound source path', 'sound source', 'estimated'])
+
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=60)
+
 
 plt.show()
+
+ani.save('spiral.mp4', writer=writer)
